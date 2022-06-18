589383c56f6bd664e88912d1805987b21c4bbdc0
Jesus Urbina, PC Gamer <jsidereus@gmail.com>
2022-06-17 22:00:00 -0500

    Ejercicios de Operadores Relacionales

---
+++ Operadores Relacionales.py
@@ -0,0 +1,17 @@
+# Operadores relacionales
+
+# Operador menor que
+resultado = 10 < 20
+print ("El resultado de Operador Menor que es ", resultado)
+
+# Operador mayor que
+resultado = 10 > 20
+print ("El resultado de Operador Mayor que es ", resultado)
+
+# Operador de Igualdad
+resultado = 10 == 20
+print ("El resultado de Operador Igualdad es ", resultado)
+
+# Operador de Diferencia
+resultado = 10 != 10
+print ("El resultado de Operador Diferencia es ", resultado)
